36ece4d200414bf145ea300294c45a6df81eaab2
DavidRacz90 <davidracz90@gmail.com>
2018-10-29 11:11:21 +0100

    coding hours

---
+++ week-02/day-01/coding-hours.ts
@@ -0,0 +1,7 @@
+'use strict';
+
+var codingHours = (17*5)*6;
+console.log("We spent " + codingHours + " hours with coding");
+
+var totalHours = 17*52;
+console.log("The precentage of the coding hours is " + ((codingHours/totalHours)*100).toFixed(0) + "%");
