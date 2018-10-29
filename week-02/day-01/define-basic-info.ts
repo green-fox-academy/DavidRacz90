0406d63cc8eb604f83437e11e7a61fcb1e5e030d
DavidRacz90 <davidracz90@gmail.com>
2018-10-29 12:02:03 +0100

    basic infos

---
+++ week-02/day-01/define-basic-info.ts
@@ -0,0 +1,10 @@
+'use strict';
+
+let myName: string = "Dávid Rácz";
+console.log(myName);
+let myAge: number = 28;
+console.log(myAge);
+let myHeight: number = 1.75;
+console.log(myHeight);
+let amIMarried: boolean = false;
+console.log(amIMarried);
