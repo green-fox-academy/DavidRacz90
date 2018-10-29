4af0b1cd8d1578aa9ab21250c47cd03e7b49ebba
DavidRacz90 <davidracz90@gmail.com>
2018-10-29 12:42:42 +0100

    variable mutations

---
+++ week-02/day-01/variable-mutation.ts
@@ -0,0 +1,43 @@
+'use strict';
+
+let a: number = 3;
+console.log(a+10);
+
+let b: number = 100;
+b - 7;
+console.log(b-7);
+
+let c: number = 44;
+console.log(c*2);
+
+let d: number = 125;
+console.log(d/5);
+
+let e: number = 8;
+console.log(e**3);
+
+let f1: number = 123
+let f2: number = 345;
+let isBiggerf1: boolean = f1 > f2;
+console.log(isBiggerf1);
+
+let g1: number = 350;
+let g2: number = 200;
+let isBiggerg1: boolean = (g2*2) > g1;
+console.log(isBiggerg1);
+
+let h: number = 1357988018575474;
+let divide11: boolean = (1357988018575474/11) == 0;
+console.log(divide11);
+
+let i1: number = 10;
+let i2:number = 3;
+let conditionsI: boolean = (i1 > (i2*2) && i1 < (i2**3));
+console.log(conditionsI);
+
+let j: number = 1521;
+let divide3And5: boolean = (j%3==0 || j%5==0);
+console.log(divide3And5);
+
+let k: string = "Apple";
+console.log(k+k+k+k);
