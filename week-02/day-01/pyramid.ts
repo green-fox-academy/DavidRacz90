600310cc00ed9dd3a7027c53d61f5b2d90a6b990
DavidRacz90 <davidracz90@gmail.com>
2018-10-29 16:54:51 +0100

    pyramid

---
+++ week-02/day-01/pyramid.ts
@@ -0,0 +1,10 @@
+"use strict";
+export{}
+let lineCount: number = 4;
+
+for(let i=1; i<= lineCount; i++){
+    let space = ' '.repeat(lineCount-i);
+    let star = '*'.repeat(i*2 -1)
+ 
+    console.log(space + star + space);
+  }
