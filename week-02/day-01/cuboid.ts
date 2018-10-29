440223e1c0540ce0eb42c533c672f1fa7947ffe6
DavidRacz90 <davidracz90@gmail.com>
2018-10-29 13:46:17 +0100

    cuboid

---
+++ week-02/day-01/cuboid.ts
@@ -0,0 +1,11 @@
+'use strict';
+
+let a: number = 10.0;
+let b: number = 12.5;
+let c: number = 8.9;
+
+let surface: number = 2*((a+b)+(a+c)+(b+c));
+console.log(surface);
+
+let volume: number = a*b*c;
+console.log(volume);
