cf018d2ddd16e4dd83eba3ae687cbf9c46c14f59
DavidRacz90 <davidracz90@gmail.com>
2018-10-29 13:51:20 +0100

    seconds in a day

---
+++ week-02/day-01/second-in-a-day.ts
@@ -0,0 +1,8 @@
+'use strict';
+
+let currentHours: number = 14;
+let currentMinutes: number = 34;
+let currentSeconds: number = 42;
+
+let remainingSeconds: number = ((24*3600)-((14*3600)+(34*60)+42));
+console.log(remainingSeconds);
