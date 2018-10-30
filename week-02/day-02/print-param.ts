c25dd91a3a67da8490d6dbd6016d68799a4a42d9
DavidRacz90 <davidracz90@gmail.com>
2018-10-30 14:50:04 +0100

    print-parameters

---
+++ week-02/day-02/print-param.ts
@@ -0,0 +1,9 @@
+// -  Create a function called `printParams`
+//    which logs to the console the input parameters
+//    (can have multiple number of arguments)
+
+function printParams(...input){
+    console.log(input);
+}
+
+printParams("ABC", "DEF", "GHJ", 1,2,3,4,5);
