59c0d6b417b45549ce26fc8699668b45edaadd7e
DavidRacz90 <davidracz90@gmail.com>
2018-11-11 22:51:08 +0100

    line play

--- Week-03/day-05/line-play.ts
+++ Week-03/day-05/line-play.ts
@@ -3,14 +3,20 @@
 const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d');
 
-for(let i = 0; i < 14; i++){
-    drawLines(75 +(canvas.width-75 / 14 *i), 0, canvas.width, (canvas.height-50 / 14 * i), 'purple')
-}
 
-function drawLines(xBegin: number, yBegin: number, x: number, y: number, color: string) {
-    ctx.beginPath()
-    ctx.strokeStyle = color;
-    ctx.moveTo(xBegin,yBegin)
-    ctx.lineTo(x,y);
+function lineDrawing(number: number) {
+    ctx.beginPath();
+    ctx.strokeStyle = 'purple';
+    ctx.moveTo(canvas.width, canvas.height-number);
+    ctx.lineTo(canvas.width-number, 0);
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.strokeStyle = 'green';
+    ctx.moveTo(canvas.width-number, canvas.height);
+    ctx.lineTo(0, canvas.height-number);
     ctx.stroke();
 }
+    for(let i: number = 0; i < 20; i++) {
+        lineDrawing (i * 20);
+    }
