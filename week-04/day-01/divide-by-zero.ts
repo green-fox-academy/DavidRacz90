ab547c8794aea9f1adac78ec97eefbf054e372e9
DavidRacz90 <davidracz90@gmail.com>
2018-11-12 11:46:32 +0100

    dividing by zero

---
+++ week-04/day-01/divide-by-zero.ts
@@ -0,0 +1,17 @@
+// Create a function that takes a number
+// divides ten with it,
+// and prints the result.
+// It should print 'fail' if the parameter is 0
+
+function dividing(number: number): number {
+    if (number === 0) {
+        throw ('fail');
+    }
+    return number / 10;
+}
+
+try {
+    console.log(dividing(0));
+} catch(e) {
+    console.log(e.message)
+}
