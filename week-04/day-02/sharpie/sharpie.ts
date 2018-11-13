96f432a499c7708a158ac3945fe24def3744e802
DavidRacz90 <davidracz90@gmail.com>
2018-11-13 11:45:27 +0100

    sharpie

---
+++ week-04/day-02/sharpie/sharpie.ts
@@ -0,0 +1,32 @@
+// // Create Sharpie class
+// // We should know about each sharpie their color (which should be a string), 
+// width (which will be a floating point number),
+//  inkAmount (another floating point number)
+// // When creating one, we need to specify the color and the width
+// // Every sharpie is created with a default 100 as inkAmount
+// // We can use() the sharpie objects
+// // which decreases inkAmount
+
+export { }
+
+class Sharpie {
+    color: string;
+    width: number;
+    inkAmount: number = 100.0;
+
+
+    constructor(color, width) {
+        this.color = color;
+        this.width = width;
+    }
+    use() {
+        this.inkAmount = this.inkAmount-this.width;
+        return this.inkAmount;
+    }
+}
+
+let newSharpie = new Sharpie('green', 47.3);
+
+
+newSharpie.use();
+console.log(newSharpie);
