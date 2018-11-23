3074e9977ca1a77dd802c1eb35f581cad7837068
DavidRacz90 <davidracz90@gmail.com>
2018-11-23 14:28:34 +0100

    fixed fibonacci

--- week-05/day-04/fibonacci/fibonacci.ts
+++ week-05/day-04/fibonacci/fibonacci.ts
@@ -1,14 +1,24 @@
-export function fibonacci(number: any): number {
+export function fibonacci(inputNumber: any): number {
   
-  let a = 1, b = 0, temp;
-
-  while (number >= 0) {
-    temp = a;
-    a = a + b;
-    b = temp;
-    number--;
+  try {
+    if (typeof (inputNumber) === 'number') {
+      throw new Error('The input is not a number')
+    }
+  } catch (error) {
+    throw error.message
   }
-
-  return b;
+  try {
+    if (inputNumber < 0) {
+      throw new Error('The input is not a positive number')
+    }
+  } catch (error) {
+    throw error.message
+  }
+  if (inputNumber === 0) {
+    return 0;
+  } else if (inputNumber === 1) {
+    return 1
+  } else if (inputNumber > 1) {
+    return fibonacci(inputNumber - 2) + fibonacci(inputNumber - 1)
+  }
 }
-
