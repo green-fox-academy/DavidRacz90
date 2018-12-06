996d6f177d2de1e16c3811226bdcd9a58a7ca530
DavidRacz90 <davidracz90@gmail.com>
2018-12-06 14:48:00 +0100

    task 6 image styling

--- week-07/day-02/exercise6/main.js
+++ week-07/day-02/exercise6/main.js
@@ -14,3 +14,12 @@ let heading = document.createElement('h1');
 heading.textContent = 'I can add elements to the DOM!';
 container.appendChild(heading);
 
+let img = document.createElement('img');
+
+img.setAttribute('src', 'https://cdn.logfurnitureplace.com/media/catalog/product/cache/1/image/750x/9df78eab33525d08d6e5fb8d27136e95/w/i/wild-woods-166.jpg');
+container.appendChild(img);
+img.style.width = '300px';
+img.style.height = '300px';
+img.style.border = '2px solid black';
+img.style.borderRadius = '25px';
+
