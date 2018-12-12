0768c664a15dc2c28cb8732c01833b88a263265e
DavidRacz90 <davidracz90@gmail.com>
2018-12-12 14:46:59 +0100

    feat(bubbles) naming variables

--- week-08/day-03/bubbling/bubbling.js
+++ week-08/day-03/bubbling/bubbling.js
@@ -1,7 +1,7 @@
 const buttons = document.querySelector('nav');
 const image = document.querySelector('.img-inspector');
-let xPosition = 0;
-let yPosition = 0;
+let x = 0;
+let y = 0;
 let size = 200;
 
 buttons.addEventListener('click', e => {
@@ -13,20 +13,20 @@ buttons.addEventListener('click', e => {
     size -= 10;
     image.style.backgroundSize = `${size}%`;
   }
-  if (e.target.dataset.direction === "left" && xPosition > 0) {
-    xPosition -= 10;
-    image.style.backgroundPosition = `${xPosition}% ${yPosition}%`;
+  if (e.target.dataset.direction === "left" && x > 0) {
+    x -= 10;
+    image.style.backgroundPosition = `${x}% ${y}%`;
   }
-  if (e.target.dataset.direction === "right" && xPosition < 100) {
-    xPosition += 10;
-    image.style.backgroundPosition = `${xPosition}% ${yPosition}%`;
+  if (e.target.dataset.direction === "right" && x < 100) {
+    x += 10;
+    image.style.backgroundPosition = `${x}% ${y}%`;
   }
-  if (e.target.dataset.direction === "up" && yPosition > 0) {
-    yPosition -= 10;
-    image.style.backgroundPosition = `${xPosition}% ${yPosition}%`;
+  if (e.target.dataset.direction === "up" && y > 0) {
+    y -= 10;
+    image.style.backgroundPosition = `${x}% ${y}%`;
   }
-  if (e.target.dataset.direction === "down" && yPosition < 100) {
-    yPosition += 10;
-    image.style.backgroundPosition = `${xPosition}% ${yPosition}%`;
+  if (e.target.dataset.direction === "down" && y < 100) {
+    y += 10;
+    image.style.backgroundPosition = `${x}% ${y}%`;
   }
 });
