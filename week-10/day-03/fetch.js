eb0fb185cdd580fbbe34866a127942e80781c59c
DavidRacz90 <davidracz90@gmail.com>
2019-01-09 12:30:05 +0100

    fetch

--- week-10/day-03/fetch.js
+++ week-10/day-03/fetch.js
@@ -3,13 +3,12 @@ const URL = 'http://api.icndb.com/jokes/random';
 const button = document.createElement('button');
 button.innerHTML = 'Gimme a joke';
 document.body.appendChild(button)
-button.addEventListener('click', (event) => {
+button.addEventListener('click', () => {
   fetch(URL)
     .then(response => response.json())
-    .then(response => {
-      const joke = response.value.joke;
+    .then(response => { 
       const paragraph = document.createElement('p');
-      paragraph.innerText = joke;
+      paragraph.innerText = response.value.joke;
       document.body.appendChild(paragraph);
     })
 });
