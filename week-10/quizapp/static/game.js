b55d975659114a9c32a3b87fbc73a816fed3ddf1
DavidRacz90 <davidracz90@gmail.com>
2019-01-09 16:44:38 +0100

    feat(quiz-app)display a question and check is it correct

--- week-10/quizapp/static/game.js
+++ week-10/quizapp/static/game.js
@@ -5,7 +5,27 @@ xhr.setRequestHeader('Content-Type', 'application/json');
 xhr.onload = () => {
   if(xhr.status >= 200 && xhr.status < 400) {
     const res = JSON.parse(xhr.responseText);
-    
+    console.log(res);
+    const question = document.createElement('h3');
+    question.innerText = res[0].question;
+    document.body.appendChild(question);
+    let score = 0;
+    const scoreBoard = document.createElement('p');
+    scoreBoard.innerText = `Score: ${score}`;
+    document.body.appendChild(scoreBoard)
+    res.forEach(e => {
+      const answer = document.createElement('button');
+      answer.innerText = e.answer;
+      document.body.appendChild(answer);
+      answer.addEventListener('click', () => {
+        if(e.is_correct === 1) {
+          score++;
+          console.log('yay');
+        } else {
+          console.log('nay');
+        }
+      })
+    });
   }
 }
 xhr.send();
